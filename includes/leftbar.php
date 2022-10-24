3021442c489ddef280b0ce860f19132f1f8a3895
Noel <112729963+NoelH98@users.noreply.github.com>
2022-10-24 09:29:20 +0300

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,84 @@
+<div class="left-sidebar bg-blue-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="ADMIN" class="img-circle profile-img">
+                                <h6 class="title">Admin</h6>
+                                <small class="info">DEBA SYSTEM</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Manage</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Team</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="addWrite.php"><i class="fa fa-bars"></i> <span>Add</span></a></li>
+                                            <li><a href="addPDF.php"><i class="fa fa fa-server"></i> <span>Manage</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Blogs</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Create post</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Upload PDF/WORD</span></a></li>
+											 <li><a href="manage-blogs.php"><i class="fa fa fa-server"></i> <span>Manage Posts</span></a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Users</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Users</span></a></li>
+                                             <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Approve user</span></a></li>
+										    <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Change subscription fee</span></a></li>
+                                        </ul>
+                                    </li>
+									<li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Partners</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage partners</span></a></li>
+                                             <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Add partners</span></a></li>
+	
+                                        </ul>
+                                    </li>
+									<li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Workshop</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Add workshop</span></a></li>
+                                             <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Remove workshop</span></a></li>
+                                        </ul>
+                                    </li>
+									<li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Gallery</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Add picture</span></a></li>
+                                             <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Remove picture</span></a></li>
+										    
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Site stats</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Misc</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>User Logs</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="editCred.php"><i class="fa fa fa-server"></i> <span> Change Admin Credentials</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
